f83b4c13155a688f280c74582b9bbe492a6d0f89
kaylubr <kylebreyes092005@gmail.com>
2025-11-22 17:56:07 +0800

    Connect server to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const { MONGODB_URI } = require('./utils/config')
 
 const app = express()
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log(`Connection successful`))
+  .catch(error => console.error(error))
 
 module.exports = app
